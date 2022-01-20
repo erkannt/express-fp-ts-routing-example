31b30d99b1b4cbee63f7ed30995ea249902965b4
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-01-20 17:17:23 +0000

    get hello world working

--- index.ts
+++ index.ts
@@ -1,13 +1,27 @@
-import * as Express from "express";
- 
-const app = Express.default();
+import * as P from "fp-ts-routing";
+import { pipe } from "fp-ts/function";
+import * as O from "fp-ts/Option";
+import express from 'express'
 
-app.get("/", (req, res, next) => {
-  res.send('hello world');
+const app = express();
+
+const homeMatch = P.end;
+
+
+app.get("*", (req, res) => {
+  pipe(
+    req.originalUrl,
+    (url) => P.parse(homeMatch.parser.map(O.some), P.Route.parse(url), O.none),
+    O.fold(
+      () => {
+        res.status(404);
+        res.send("Not found");
+      },
+      () => {
+        res.send('hello world');
+      }
+    )
+  );
 });
- 
-app.get("/search/:query", (req, res, next) => {
-  res.send(`Search query: ${req.params.query}`);
-});
- 
+
 app.listen(8080);
