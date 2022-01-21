d04e86c91866826681b37a52b4b880ac70c1774d
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-01-21 17:03:25 +0000

    param with regex guarded brand

--- index.ts
+++ index.ts
@@ -14,17 +14,37 @@ const homePage = (): string => ('hello world')
 
 // with a query: /search?query=foobar
 const searchMatch = P.lit('search')
-.then(P.query(t.strict({ query: t.string })))
-.then(P.end)
+  .then(P.query(
+    t.strict({ query: t.string })
+    ))
+  .then(P.end)
 
 type SearchParams = { query: string }
 
 const searchPage = (params: SearchParams): string => (`Search query: ${params.query}`)
 
+// with regex guarded param: /groups/:slug(${groupSlugRegex})/evaluated-articles
+const groupSlugCodec = t.brand(
+  t.string,
+  (input): input is t.Branded<string, { readonly GroupSlug: unique symbol }> => /^[A-Za-z0-9-]{0,30}$/.test(input),
+  'GroupSlug'
+)
+
+type GroupSlug = t.TypeOf<typeof groupSlugCodec>
+
+const evaluatedArticlesMatch = P.lit('groups')
+  .then(P.type('slug', groupSlugCodec))
+  .then(P.lit('evaluated-articles'))
+
+type EvaluatedArticlesParams = {slug: GroupSlug}
+
+const evaluatedArticlesPage = (params: EvaluatedArticlesParams): string => (`${params.slug} is a valid group slug`)
+
 const router = pipe(
   [
     homeMatch.parser.map(homePage),
     searchMatch.parser.map(searchPage),
+    evaluatedArticlesMatch.parser.map(evaluatedArticlesPage)
   ],
   M.concatAll(P.getParserMonoid())
 )
