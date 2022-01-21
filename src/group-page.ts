6250d7505cf9a0d3034fdc4006bcb511ea57e84e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-01-21 17:16:38 +0000

    housekeeping

--- src/group-page.ts
+++ src/group-page.ts
@@ -1,4 +1,4 @@
-import * as t from 'io-ts';
+import * as t from 'io-ts'
 
 export const groupSlugCodec = t.brand(
   t.string,
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import { evaluatedArticlesPage, groupSlugCodec } from './group-page';
-import * as P from "fp-ts-routing";
-import { pipe } from "fp-ts/function";
+import { evaluatedArticlesPage, groupSlugCodec } from './group-page'
+import * as P from 'fp-ts-routing'
+import { pipe } from 'fp-ts/function'
 import express from 'express'
-import * as O from 'fp-ts/Option';
+import * as O from 'fp-ts/Option'
 import * as M from 'fp-ts/Monoid'
-import * as t from 'io-ts';
-import { homePage } from './home-page';
-import { searchPage } from './search-page';
+import * as t from 'io-ts'
+import { homePage } from './home-page'
+import { searchPage } from './search-page'
 
-const app = express();
+const app = express()
 
 // basic route: /
-const homeMatch = P.end;
+const homeMatch = P.end
 
 // with a query: /search?query=foobar
 const searchMatch = P.lit('search')
   .then(P.query(
     t.strict({ query: t.string })
-    ))
+  ))
   .then(P.end)
 
 // with regex guarded param: /groups/:slug(${groupSlugRegex})
@@ -34,19 +34,19 @@ const router = pipe(
   M.concatAll(P.getParserMonoid())
 )
 
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
   pipe(
     req.originalUrl,
     P.Route.parse,
     router.run,
     O.match(
       () => {
-        res.status(404);
-        res.send("Not found");
+        res.status(404)
+        res.send('Not found')
       },
       ([body]) => res.send(body)
     )
-  );
-});
+  )
+})
 
-app.listen(8080);
+app.listen(8080)
