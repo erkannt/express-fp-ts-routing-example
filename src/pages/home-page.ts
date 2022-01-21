524cf879b46ff85ab38d077b1add4063f6f852fe
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-01-21 20:15:03 +0000

    use formatters on home page

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -1,1 +1,11 @@
-export const homePage = (): string => ('hello world')
+import { format } from 'fp-ts-routing'
+import { groupsPageMatch, searchMatch } from '../matches'
+
+export const homePage = (): string => (`
+  <h1>fp-ts-routing and Express</h1>
+
+  <ul>
+    <li><a href=${format(groupsPageMatch.formatter, {})}>Groups page</a></li>
+    <li><a href=${format(searchMatch.formatter, {query: 'test'})}>Example search</a></li>
+  </ul>
+`)
