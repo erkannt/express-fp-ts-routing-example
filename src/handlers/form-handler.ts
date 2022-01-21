65400c4ea55e424412e887650770f923c12ad2dd
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-01-21 20:59:42 +0000

    wip: formHandler (can't figure out how to pass in req.body)

--- src/handlers/form-handler.ts
+++ src/handlers/form-handler.ts
@@ -1,1 +1,35 @@
-export const formHandler = (): string => ('success')
+import { pipe } from 'fp-ts/lib/function'
+import * as t from 'io-ts'
+import * as E from 'fp-ts/Either'
+import * as PR from 'io-ts/PathReporter'
+
+const renderSuccess = (viewModel: Input) => `
+  Form received:
+
+  Name: ${viewModel.name}
+  Favourite colour: ${viewModel.color}
+`
+
+const renderError = (error: string) => `
+  Form invalid:
+
+  ${error}
+`
+
+const InputC = t.type({
+  name: t.string,
+  color: t.string,
+})
+
+type Input = t.TypeOf<typeof InputC>
+
+export const formHandler = (input: unknown): string => pipe(
+  input,
+  (foo) => {console.log('>>>>>>>>>>>>', foo); return foo},
+  InputC.decode,
+  E.mapLeft((errors) => PR.failure(errors).join('\n')),
+  E.match(
+    renderError,
+    renderSuccess,
+  )
+)
