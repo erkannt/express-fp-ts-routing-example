c9969963d3f9beb410f61c3b7faca4bde85fd2fe
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-01-21 19:55:05 +0000

    try making matches easiert to read

--- src/matches.ts
+++ src/matches.ts
@@ -1,19 +1,12 @@
 import { GroupSlugC } from './pages/group-page'
-import * as P from 'fp-ts-routing'
-import * as t from 'io-ts'
-
+import {lit, query, end, type} from 'fp-ts-routing'
+import { SearchParamsC } from './pages/search-page'
 
 // basic route: /
-export const homeMatch = P.end
+export const homeMatch = end
 
 // with a query: /search?query=foobar
-export const searchMatch = P.lit('search')
-  .then(P.query(
-    t.strict({ query: t.string })
-  ))
-  .then(P.end)
+export const searchMatch = lit('search').then(query(SearchParamsC)).then(end)
 
 // with regex guarded param: /groups/:slug(${groupSlugRegex})
-export const groupPageMatch = P.lit('groups')
-  .then(P.type('slug', GroupSlugC))
-  .then(P.lit('evaluated-articles'))
+export const groupPageMatch = lit('groups').then(type('slug', GroupSlugC)).then(lit('evaluated-articles'))
--- src/pages/search-page.ts
+++ src/pages/search-page.ts
@@ -1,3 +1,7 @@
-type Params = { query: string }
+import * as t from 'io-ts'
+
+export const SearchParamsC = t.strict({ query: t.string })
+
+type Params = t.TypeOf<typeof SearchParamsC>
 
 export const searchPage = (params: Params): string => (`Search query: ${params.query}`)
