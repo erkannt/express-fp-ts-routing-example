ff6ae72f71fdebdbf11885e2c74739728acbd103
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-01-25 17:32:24 +0000

    Explain what the pages showcase

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -5,8 +5,8 @@ export const homePage = (): string => (`
   <h1>fp-ts-routing and Express</h1>
 
   <ul>
-    <li><a href=${format(groupsMatch.formatter, {})}>Groups page</a></li>
-    <li><a href=${format(searchMatch.formatter, {query: 'test'})}>Example search</a></li>
-    <li><a href=${format(formInputMatch.formatter, {})}>Form input</a></li>
+    <li><a href=${format(searchMatch.formatter, {query: 'test'})}>Example search</a> (must contain a certain param)</li>
+    <li><a href=${format(groupsMatch.formatter, {})}>Groups page</a> (part of url must match a regex)</li>
+    <li><a href=${format(formInputMatch.formatter, {})}>Form input</a> (validation an parsing of body)</li>
   </ul>
 `)
